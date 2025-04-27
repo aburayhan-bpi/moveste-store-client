8779ba725923c3d5ab2ec64ff3020e894cef3f9d
Abu Rayhan <aburayhan.bpi@gmail.com>
2025-04-28 00:28:57 +0600

    Scroll-linked animations

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router"; // Small fix: react-router-dom
 import Navbar from "../components/common/Navbar/Navbar";
 import Footer from "../components/common/Footer/Footer";
+import { motion, useScroll } from "framer-motion"; // Correct import!
+
 const RootLayout = () => {
+  const { scrollYProgress } = useScroll();
+
   return (
     <div>
-      {/* Navbar Section */}
+      {/* Scroll Progress Bar */}
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "4px",
+          transformOrigin: "left",
+          backgroundColor: "#47aaf5ec",
+          zIndex: 50,
+        }}
+      />
+
+      {/* Navbar */}
       <Navbar />
-      {/* Outlet */}
+
+      {/* Main Content */}
       <Outlet />
+
       {/* Footer */}
       <Footer />
     </div>
